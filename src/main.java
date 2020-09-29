a3da14dd24af2c5c9a64c32d5bed83600b5dfbda
Skitovich Ruslan <zloycvetok@gmail.com>
2020-09-29 15:19:06 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        double regularBonus = 10.4;
+        double specialBonus = 5.2;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
